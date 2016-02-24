f6e627f3466f695d919217d361198c0763d3945b
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2016-02-24 14:38:00 +0000

    Added constants and made sub-classes private

--- deploy-core/src/main/java/uk/laxd/deepweb/lang/Constants.java
+++ deploy-core/src/main/java/uk/laxd/deepweb/lang/Constants.java
@@ -5,36 +5,35 @@ package uk.laxd.deepweb.lang;
  */
 public class Constants {
 
+    private Constants(){}
+
     public final class RequestMappings {
+        private RequestMappings(){}
         public static final String BUILD_FLOW = "/flow";
     }
 
     public class DB {
 
+        private DB(){}
+
         public static final String ID_COLUMN = "ID";
 
         public class BuildFlow {
+            private BuildFlow(){}
             public static final String NAME_COLUMN = "NAME";
         }
 
         public class BuildFlowStep {
+            private BuildFlowStep(){}
             public static final String BUILD_FLOW_ID_COLUMN = "BUILD_FLOW_ID";
             public static final String TYPE_COLUMN = "TYPE";
         }
-    }
 
-    public class SQL {
-
-        public static final String SELECT_SERVER = "SELECT SERVER.ID AS SERVER_ID," +
-                " SERVER.NAME," +
-                " SERVER.HOST," +
-                " CREDENTIALS.ID AS CREDENTIALS_ID," +
-                " CREDENTIALS.USERNAME," +
-                " CREDENTIALS.PASSWORD," +
-                " CREDENTIALS.KEY_LOC," +
-                " CREDENTIALS.TYPE " +
-                " FROM SERVER" +
-                " INNER JOIN CREDENTIALS" +
-                " ON CREDENTIALS.ID = SERVER.CREDENTIALS_ID";
+        public class BuildFlowStepArgument {
+            private BuildFlowStepArgument(){}
+            public static final String NAME_COLUMN = "NAME";
+            public static final String VALUE_COLUMN = "VALUE";
+            public static final String BUILD_FLOW_STEP_ID_COLUMN = "BUILD_FLOW_STEP_ID";
+        }
     }
 }
