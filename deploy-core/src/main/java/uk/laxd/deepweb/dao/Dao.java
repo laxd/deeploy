389d70641a45076b5f983017b599f160d5b97cbf
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2016-02-24 11:57:26 +0000

    Updated Dao to have Long as findById argument

--- deploy-core/src/main/java/uk/laxd/deepweb/dao/Dao.java
+++ deploy-core/src/main/java/uk/laxd/deepweb/dao/Dao.java
@@ -6,6 +6,6 @@ import java.util.List;
  * Created by Lenny on 27/09/2015.
  */
 public interface Dao<T> {
-    T findById(int id);
+    T findById(Long id);
     List<T> findByRestrictions(Restrictions restriction);
 }
