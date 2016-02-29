650b1466ea2c209bc8fccc73c23fecc7f78e7acf
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2016-02-29 15:59:47 +0000

    Added maven buidl flow step executor

---
+++ deploy-core/src/main/java/uk/laxd/deepweb/executor/MvnBuildFlowStepExecutor.java
@@ -0,0 +1,35 @@
+package uk.laxd.deepweb.executor;
+
+import org.apache.maven.shared.invoker.*;
+import uk.laxd.deepweb.plugin.BuildFlowStepExecutor;
+import uk.laxd.deepweb.plugin.ExecutionResult;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Map;
+
+/**
+ * Created by lawrence on 29/02/16.
+ */
+public class MvnBuildFlowStepExecutor extends BuildFlowStepExecutor {
+    @Override
+    public String getType() {
+        return "MVN";
+    }
+
+    @Override
+    public ExecutionResult executeWithArguments(Map<String, String> arguments) {
+        InvocationRequest request = new DefaultInvocationRequest();
+
+        request.setPomFile(new File(arguments.get("WORKSPACE"), "pom.xml"));
+        request.setGoals(Arrays.asList("clean", "compile"));
+
+        Invoker invoker = new DefaultInvoker();
+        try {
+            invoker.execute(request);
+            return new ExecutionResult(0);
+        } catch (MavenInvocationException e) {
+            return new ExecutionResult(1, e.getMessage());
+        }
+    }
+}
