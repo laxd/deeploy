b8443b7fb1212d189e31195e88023a31a97272f7
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2016-03-16 16:33:26 +0000

    Updated js to only allow dragging from executors -> steps

--- deploy-web/src/main/resources/static/js/main.js
+++ deploy-web/src/main/resources/static/js/main.js
@@ -3,40 +3,38 @@ $(document).ready(function() {
 		console.log("Showing add build flow step dialog...");
 	});
 
-	//$(".executor").on("click", function() {
-	//	var url = "/modal/executor?type=SSH";
-	//	$.ajax({
-	//		url: url,
-	//		success: function(data) {
-	//			$("#modal").replace(data);
-    //
-	//			$("#modal").dialog({
-	//				modal: true,
-	//				show: true
-	//			})
-	//		}
-	//	})
-	//});
-
-	$("#steps, #executors").sortable({
-		connectWith: "#steps",
+	$("#steps").sortable({
+		revert: true,
+		accept: "#executor",
 		receive: showAddBuildFlowModal
+	});
+
+	$(".executor").draggable({
+		connectToSortable: "#steps",
+		helper: "clone"
 	}).disableSelection();
 
-	//$(".executor").draggable({
-	//	cursor: 'move',
-	//	revert: true,
-	//	clone: true
-	//});
-    //
-	//$("#add-new-build-flow-step").droppable({
-	//	accept: ".executor",
-	//	drop: showAddBuildFlowModal
-	//});
 });
 
 function showAddBuildFlowModal(event, ui) {
 	var buildFlowType = ui.item.data('type');
 
 	console.log("Showing modal for " + buildFlowType);
+
+	var url = "/modal/executor?type=" + buildFlowType;
+
+	// Do ajax to get modal
+	$.ajax({
+		url: url,
+		success: function(data) {
+			var modal = $("#modal");
+			modal.replace(data);
+			modal.attr("hidden", false);
+			modal.dialog({
+				modal: true,
+				show: true
+			})
+		}
+	});
+
 }
