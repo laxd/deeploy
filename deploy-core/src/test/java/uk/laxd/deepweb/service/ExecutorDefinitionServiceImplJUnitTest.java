ffe3c0b959d841beaecc488dae5beeb5ec531319
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2016-04-15 11:13:09 +0100

    Tidied up test-context and fixed ExecutorDefinitionService test

--- deploy-core/src/test/java/uk/laxd/deepweb/service/ExecutorDefinitionServiceImplJUnitTest.java
+++ deploy-core/src/test/java/uk/laxd/deepweb/service/ExecutorDefinitionServiceImplJUnitTest.java
@@ -1,87 +1,61 @@
 package uk.laxd.deepweb.service;
 
-import com.j256.ormlite.dao.Dao;
-import com.j256.ormlite.spring.DaoFactory;
-import com.j256.ormlite.support.ConnectionSource;
-import com.j256.ormlite.table.TableUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import uk.laxd.deepweb.executor.ApplicationContextBuildFlowStepExecutorFactory;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import uk.laxd.deepweb.executor.BuildFlowStepExecutorFactory;
 import uk.laxd.deepweb.model.BuildFlowStep;
-import uk.laxd.deepweb.model.BuildFlowStepArgument;
-import uk.laxd.deepweb.plugin.Executor;
+import uk.laxd.deepweb.plugin.*;
+
+import java.util.Map;
+
+import static org.mockito.Matchers.anyMap;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by lawrence on 02/03/16.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:spring/test-context.xml")
+@RunWith(MockitoJUnitRunner.class)
 public class ExecutorDefinitionServiceImplJUnitTest {
 
-    @Autowired
-    private ConnectionSource connectionSource;
+    @InjectMocks
+    private BuildFlowStepExecutorService buildFlowStepExecutorService = new BuildFlowStepExecutorServiceImpl();
 
-    @Autowired
-    private BuildFlowStepExecutorService buildFlowStepExecutorService;
+    @Mock
+    private BuildFlowStepExecutorFactory buildFlowStepExecutorFactory;
 
-    @Autowired
-    private ApplicationContextBuildFlowStepExecutorFactory executorFactory;
+    @Mock
+    private BuildFlowStepArgumentService buildFlowStepArgumentService;
+
+    @Mock
+    private PluginManager pluginManager;
+
+    @Mock
+    private ExecutorDefinition executorDefinition;
 
     @Before
     public void setUp() throws Exception {
-        TableUtils.dropTable(connectionSource, Executor.class, true);
-        TableUtils.createTable(connectionSource, Executor.class);
-
-        TableUtils.dropTable(connectionSource, BuildFlowStepArgument.class, true);
-        TableUtils.createTable(connectionSource, BuildFlowStepArgument.class);
-
-        TableUtils.dropTable(connectionSource, BuildFlowStep.class, true);
-        TableUtils.createTable(connectionSource, BuildFlowStep.class);
-
-        Dao<Executor, Long> executorDao = DaoFactory.createDao(connectionSource, Executor.class);
-        Dao<BuildFlowStep, Long> buildFlowStepDao = DaoFactory.createDao(connectionSource, BuildFlowStep.class);
-        Dao<BuildFlowStepArgument, Long> buildFlowStepArgumentDao = DaoFactory.createDao(connectionSource, BuildFlowStepArgument.class);
-
         Executor executor = new Executor();
-        executor.setName("Test Executor");
-        executorDao.create(executor);
+        executor.setName("Executor Name");
+        PluginDefinition pluginDefinition = new PluginDefinition(executor);
 
-        BuildFlowStep buildFlowStep = new BuildFlowStep();
-        buildFlowStep.setExecutorName("Test Executor");
-        buildFlowStepDao.create(buildFlowStep);
+        when(pluginManager.getPluginDefinition("Executor Name")).thenReturn(pluginDefinition);
 
-        BuildFlowStepArgument buildFlowStepArgument1 = new BuildFlowStepArgument();
-        buildFlowStepArgument1.setName("TEST_ARGUMENT_1");
-        buildFlowStepArgument1.setValue("TEST_VALUE_1");
-        buildFlowStepArgument1.setBuildFlowStep(buildFlowStep);
-        buildFlowStepArgumentDao.create(buildFlowStepArgument1);
-
-        BuildFlowStepArgument buildFlowStepArgument2 = new BuildFlowStepArgument();
-        buildFlowStepArgument2.setName("TEST_ARGUMENT_2");
-        buildFlowStepArgument2.setValue("TEST_VALUE_2");
-        buildFlowStepArgument2.setBuildFlowStep(buildFlowStep);
-        buildFlowStepArgumentDao.create(buildFlowStepArgument2);
-
-        DefaultListableBeanFactory parentBeanFactory = new DefaultListableBeanFactory();
-
-        GenericApplicationContext parentContext =
-                new GenericApplicationContext(parentBeanFactory);
-
-        parentContext.refresh();
-
-        executorFactory.setApplicationContext(parentContext);
+        when(buildFlowStepExecutorFactory.createExecutor("Executor Name")).thenReturn(executorDefinition);
     }
 
     @Test
-    public void testExecutorExecutedIsTest() throws Exception {
-        BuildFlowStep buildFlowStep = DaoFactory.createDao(connectionSource, BuildFlowStep.class).queryForId(1L);
+    public void testExecutorServiceExecutesExecutor() throws Exception {
+        BuildFlowStep buildFlowStep = new BuildFlowStep();
+        buildFlowStep.setExecutorName("Executor Name");
 
         buildFlowStepExecutorService.execute(buildFlowStep);
+
+        verify(executorDefinition).executeWithArguments(anyMap());
     }
 }
--- deploy-plugin/src/main/java/uk/laxd/deepweb/plugin/PluginRegistry.java
+++ deploy-plugin/src/main/java/uk/laxd/deepweb/plugin/PluginRegistry.java
@@ -1,11 +1,16 @@
 package uk.laxd.deepweb.plugin;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.*;
 
 /**
  * Created by lawrence on 14/03/16.
  */
 public class PluginRegistry {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PluginRegistry.class);
 
     private Collection<PluginDefinition> plugins = new ArrayList<>();
 
@@ -14,6 +19,8 @@ public class PluginRegistry {
     }
 
     public PluginDefinition getPlugin(String name) {
+        LOGGER.info("Retrieving plugin '{}'", name);
+
         return plugins.stream()
             .filter(def -> def.getExecutor().getName().equals(name))
             .findFirst()
