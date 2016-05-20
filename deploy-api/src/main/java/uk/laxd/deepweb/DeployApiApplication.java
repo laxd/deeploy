ff05ceaca17e74319e97cc11a7d99bef48c13b44
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2016-05-20 10:13:09 +0100

    Removed dao-config.xml resource import from DeployApiApplication.java

--- deploy-api/src/main/java/uk/laxd/deepweb/DeployApiApplication.java
+++ deploy-api/src/main/java/uk/laxd/deepweb/DeployApiApplication.java
@@ -2,10 +2,8 @@ package uk.laxd.deepweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:dao-config.xml")
 public class DeployApiApplication {
 
 	public static void main(String[] args) {
